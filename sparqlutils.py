f9a0a14300eaeb4426e248641063e6490daeb2f8
Barman Raphael <barman.raphael@gmail.com>
2017-05-08 15:42:31 +0200

    Information getter done

--- sparqlutils.py
+++ sparqlutils.py
@@ -1,10 +1,15 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
+import io
+import json
 import re
+import urllib
 
 sparql = SPARQLWrapper("http://iccluster052.iccluster.epfl.ch:8899/sparql")
 sparql.setReturnFormat(JSON)
 
+journals = {'JDG': "Journal de Genève", "GDL": "Gazette de Lausanne"}
 
+# Executes a query on the sparql endpoint and returns a flattened list of results
 def execute_query(query):
     sparql.setQuery(query)
     results = []
@@ -16,6 +21,7 @@ def execute_query(query):
     return results
 
 
+# Takes an artComp and sorlId and transforms it into a valid letempsarchives.ch url
 def id2url(artComp, solrId):
     journal, day, month, year, page = re.sub(
         r"http://localhost:8080/letemps-data/(...)_(.*?)-(.*?)-(.*?)_Ar0(..).*",
@@ -24,38 +30,48 @@ def id2url(artComp, solrId):
         journal, year, month, day, page, solrId)
     return url
 
+
+# Cleans a person name
+def cleanName(name):
+    # We remove every character followed by a '.'
+    return re.sub('.\.','',name).title().strip()
+
 persons = {}
+
+# Adds a mention for a person
+def addMention(mention):
+    global persons
+    person = cleanName(mention['name'])
+    # We do not keep person with only a name/surname
+    if len(person.split(' ')) < 2:
+        return
+    mention['name'] = person
+    if person not in persons:
+        persons[person] = []
+    persons[person].append(mention)
+
 
 
 query = """
-SELECT STR(?id) as ?id STR(?issue) AS ?journal STR(?date) AS ?date STR(?name) AS ?personName STR(?firstname) AS ?firstname STR(?lastname) AS ?lastname STR(?function) AS ?function STR(?functionType) AS ?functiontype STR(?nationality) AS ?nationality ?artComp ?solrId
-WHERE
+SELECT STR(?name) AS ?personName STR(?functionType) AS ?functiontype WHERE
 {
 ?pm a lt-owl:PersonMention .
 ?pm lt-owl:name ?name .
-OPTIONAL {?pm lt-owl:firstname ?firstname }  
-OPTIONAL {?pm lt-owl:lastname ?lastname } 
-?pm lt-owl:function ?function .
 ?pm lt-owl:functionType ?functionType .
 ?pm lt-owl:nationality ?nationality .
-?pm lt-owl:mentionedIn ?artComp .
-?artComp lt-owl:issueDate ?date .
-?artComp lt-owl:publication ?issue .
-?artComp lt-owl:articleId ?id.
-?artComp dct:title ?title .
-?artComp lt-owl:solrId ?solrId
 FILTER (!contains(?functionType, "other"))
 FILTER (contains(lcase(str(?nationality)), "suisse"))
 }
 LIMIT 100
 """
+
 query2 = """
-SELECT  STR(?issue) AS ?issue STR(?date) AS ?date ?artComp ?solrId ?f ?functionType ?name
+SELECT  STR(?issue) AS ?journal STR(?date) AS ?date ?artComp ?solrId ?function ?functionType ?name
 WHERE
 {
         ?p a lt-owl:PersonMention .
         ?p lt-owl:name ?name  .
-        ?p lt-owl:function ?f .
+        ?p lt-owl:function ?function .
         ?p lt-owl:functionType ?functionType .
         ?p lt-owl:mentionedIn ?artComp .
         ?artComp lt-owl:issueDate ?date .
@@ -66,13 +82,24 @@ WHERE
         FILTER (!contains(?functionType, "other"))
 }
 """
+# For each person found
 for res in execute_query(query):
-    print(query2%(res['personName']))
-    res['articles'] = execute_query(query2%(res['personName']))
-    print(len(res['articles']))
-    functions = set()
-    for article in res['articles']:
-        print(id2url(article['artComp'],article['solrId']))
-        functions.add((article['f'].title(),article['functionType']))
-    print(functions)
-    #print(id2url(res['artComp'],res['solrId']))
+    # For each article mentioning the person
+    for article in execute_query(query2%(res['personName'])):
+        article['ref'] = urllib.parse.quote(id2url(article['artComp'],article['solrId']) + "/" + cleanName(article['name'])).replace(r'%3A',':')
+        article['journal'] = journals[article['journal']]
+        article['nation'] = 'suisse'
+        article['date'] = re.sub(r"(....)-(..)-(..).*",r'\1.\2.\3',article['date'])
+        del article['artComp']
+        del article['solrId']
+        addMention(article)
+
+# We sort the mention by date
+for person, mentions in persons.items():
+    persons[person] = sorted(mentions, key=lambda k: k['date'])
+
+with io.open('persons.json', 'w', encoding='utf-8') as file:
+    json.dump(persons,file, ensure_ascii=False)
+
+
+
